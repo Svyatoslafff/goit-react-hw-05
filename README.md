d2c47afb8a21f1f9b52eda41fb07d4de22e758fa
Svyatoslafff <svjatoslavkom12@gmail.com>
2025-02-06 23:43:15 +0200

    Basic structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# goit-react-hw-05
- 
+# React HW5
---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router';
+import './App.module.scss';
+
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
